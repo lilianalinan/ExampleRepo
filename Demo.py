cd5cd142658a34581e990b9ef9f44eafda694122
Liliana Linan <lilianalinan@usc-guestwireless-upc-newsc22773.usc.edu>
2019-07-29 10:25:50 -0700

    Added demo.py

---
+++ Demo.py
@@ -0,0 +1,14 @@
+# -------------------------------------------------
+#        Name: Liliana Linan
+#        Date: July 29,2019
+#
+# Description: This is a sample python file
+# -------------------------------------------------
+
+def main():
+
+    name= "Liliana"
+    print("Hello my name is", name)
+
+if( __name__ == "__main__"):
+    main()
